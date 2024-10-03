32264db957d2a1a816f2a8a3742227433fad0402
abaidullah696 <abaidullah696@gmail.com>
2024-10-03 22:54:15 +0500

    BTC

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function HomePage() {
       <h1>Stock Trading Website</h1>
       <StockCard symbol="AAPL" />
       <StockCard symbol="GOOGL" />
+      <StockCard symbol="BTC" />
     </div>
   );
 }
